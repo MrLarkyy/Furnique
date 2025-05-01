a893d88303c393d90a4810c0a4722df61051d9e3
MrLarkyy_ <studiotadikl@gmail.com>
2025-05-01 23:52:15 +0200

    Implement placeholder method for DisplayVisual creation
    Added a `create` method in `DisplayVisual` with a `TODO` placeholder. This sets up the structure for handling `FurnitureHandle` logic in the future.

--- API/src/main/kotlin/gg/aquatic/furnique/api/visual/DisplayVisual.kt
+++ API/src/main/kotlin/gg/aquatic/furnique/api/visual/DisplayVisual.kt
@@ -1,4 +1,9 @@
 package gg.aquatic.furnique.api.visual
 
+import gg.aquatic.furnique.api.furniture.FurnitureHandle
+
 class DisplayVisual: FurnitureVisual {
+    override fun create(furnitureHandle: FurnitureHandle) {
+        TODO("Not yet implemented")
+    }
 }
